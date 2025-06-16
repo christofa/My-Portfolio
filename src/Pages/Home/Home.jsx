0d0b7ef1b1f6b2f55a33c8874b9a64f1fc08734d
Christopher <christofaokoro@gmail.com>
2025-06-16 15:36:51 +0100

    Data added successfully

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import Layout from "@/components/Layout";
-import Card from "@/components/card";
+import Card from "@/components/Card";
 import HomeContact from "@/components/HomeContact";
 import { Button } from "@chakra-ui/react";
 import { Link } from "react-router";
